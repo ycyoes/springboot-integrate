1abe89f5dc1269cbb3f0f55edca11939f8af7012
ycyoes <ycyoes@163.com>
2020-05-31 10:14:34 +0800

    excel异常类

---
+++ src/main/java/com/turing/springbootintegrate/common/utils/excel/ExcelException.java
@@ -0,0 +1,28 @@
+package com.turing.springbootintegrate.common.utils.excel;
+
+
+/**
+ * Excel Exception
+ * @author ycyoes
+ * @date 2020-05-31
+ */
+public class ExcelException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ExcelException() {
+		super();
+	}
+
+	public ExcelException(String message) {
+		super(message);
+	}
+
+	public ExcelException(Throwable cause) {
+		super(cause);
+	}
+
+	public ExcelException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}

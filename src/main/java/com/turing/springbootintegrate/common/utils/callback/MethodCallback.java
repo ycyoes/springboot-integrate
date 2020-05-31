2131005eb9c7a78c3408fed592f5582d8d2ea521
ycyoes <ycyoes@163.com>
2020-05-31 14:19:17 +0800

    添加反射工具类

---
+++ src/main/java/com/turing/springbootintegrate/common/utils/callback/MethodCallback.java
@@ -0,0 +1,10 @@
+package com.turing.springbootintegrate.common.utils.callback;
+
+/**
+ * 方法回调接口
+ * @author ycyoes
+ * @date 2020-05-31
+ */
+public interface MethodCallback {
+	Object execute(Object... params);
+}
